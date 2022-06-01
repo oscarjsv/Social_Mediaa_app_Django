364eb22915cc5a593ce0782f44cad72baa41af8b
oscarjsv <gumball_junior@hotmail.com>
2022-06-01 17:32:20 -0500

    create te template to the signup

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 #configurando la primera url con mi vista index
+#configurando la primera url con mi vista signup
 app_name = 'core'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('signup', views.signup, name='signup')
 ]
