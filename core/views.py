2f762c4c58f5ae8accbc5ab1ce054ff1882cefb5
oscarjsv <gumball_junior@hotmail.com>
2022-06-22 16:56:04 -0500

    finish like port

--- core/views.py
+++ core/views.py
@@ -40,10 +40,19 @@ def like_post(request):
     post = Post.objects.get(id=post_id)
 
     like_filter = LikePost.objects.filter(post_id=post_id, username=username).first()
-
+    print(like_filter)
     if like_filter == None:
-        new_like = LikePost.objects.create(post_id=post, username=username)
+        new_like = LikePost.objects.create(post_id=post_id, username=username)
         new_like.save()
+
+        post.no_of_likes = post.no_of_likes + 1
+        post.save()
+        return redirect('/')
+    else:
+        like_filter.delete()
+        post.no_of_likes = post.no_of_likes - 1
+        post.save()
+        return redirect('/')
 
 
 @login_required(login_url='core:signin')
